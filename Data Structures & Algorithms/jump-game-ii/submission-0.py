eda8850ed7fb043c3ae560414dcd323a1f31c68b
aryanjverma <111136645+aryanjverma@users.noreply.github.com>
2026-09-17 13:02:26 -0500

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        nums[-1] = 0
+        for i in range(len(nums) - 2, - 1, - 1):
+            nums[i] = 1 + min(nums[i + 1], nums[min(len(nums) - 1, nums[i] + i)])
+        print(nums)
+        return nums[0]
